578d422d78363bbc780219afed3997bde7f6175c
Gurkirat Singh Arora <gsa119@sfu.ca>
2023-08-23 16:24:03 +0530

    .

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,10 +1,11 @@
 import { Button } from "@ui/components/button";
-
+import { Temp } from "ui";
 export default function Page() {
   return (
     <>
       <h1>Web</h1>
       <Button>Click me</Button>
+      <Temp />
     </>
   );
 }
---
+++ packages/ui/Temp.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@ui/components/ui/button";
+import { Checkbox } from "@ui/components/ui/checkbox";
+import { Card } from "@ui/components/ui/card";
+import React from "react";
+export const Temp = () => {
+  return (
+    <div>
+      <Button variant="destructive">Hello</Button>
+      <Checkbox />
+      <Card>
+        <h1>Hello </h1>
+      </Card>
+    </div>
+  );
+};
